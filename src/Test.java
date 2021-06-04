7595e58645d01c52648058595cc516cf394d3572
zhangakai <1033374069@qq.com>
2021-06-04 20:39:06 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class Test {
+    public static void main(String[] args) {
+        HttpClient
+        HttpRequest httpRequest = HttpRequest.newBuilder().GET()
+    }
+}
